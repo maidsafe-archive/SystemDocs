0310e7d0b415b75cd1e03dc5d7f623ba5aa4776c
MyName1s <viv.rajkumar@maidsafe.net>
2014-09-11 11:26:33 +0100

    Updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,51 @@
 var path = require("path");
 
 module.exports = function (grunt) {
-    grunt.loadNpmTasks('grunt-gitbook');
-    grunt.loadNpmTasks('grunt-gh-pages');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-string-replace');
+  grunt.loadNpmTasks('grunt-gitbook');
+  grunt.loadNpmTasks('grunt-gh-pages');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-string-replace');
 
-    grunt.initConfig({
-        'gitbook': {
-            development: {
-                input: "./",
-                github: "maidsafe/systemdocs"
-            }
+  grunt.initConfig({
+    'gitbook': {
+      development: {
+        input: "./",
+        github: "maidsafe/systemdocs"
+      }
+    },
+    'gh-pages': {
+      options: {
+        base: '_book'
+      },
+      src: ['**']
+    },
+    'clean': {
+      files: ['.grunt' , '_book']
+    },
+    'string-replace': {
+      dist: {
+        files: {
+          '_book/': '_book/**/style.css'
         },
-        'gh-pages': {
-            options: {
-                base: '_book'
-            },
-            src: ['**']
-        },
-        'clean': {
-            files: ['.grunt' , '_book']
-        },
-		'string-replace': {
-          dist: {
-            files: {
-              '_book/': '_book/**/style.css'
-            },
-            options: {
-              replacements: [{
-                pattern: '.book .book-body .navigation.navigation-next{right:0',
-                replacement: '.book .book-body .navigation.navigation-next{right:15px'
-              }]
-            }
-          }
-		}
-    });
+        options: {
+          replacements: [{
+            pattern: '.book .book-body .navigation.navigation-next{right:0',
+            replacement: '.book .book-body .navigation.navigation-next{right:15px'
+          }]
+        }
+      }
+    }
+  });
 
-    grunt.registerTask('publish', [
-    	'gitbook',
-		'string-replace',
-        'gh-pages',
-        'clean'
-    ]);
-	
-    grunt.registerTask('default', [
-      	'gitbook',
-      	'string-replace'
-    ]);
+  grunt.registerTask('publish', [
+    'gitbook',
+    'string-replace',
+    'gh-pages',
+    'clean'
+  ]);
+
+  grunt.registerTask('default', [
+    'gitbook',
+    'string-replace'
+  ]);
 };
